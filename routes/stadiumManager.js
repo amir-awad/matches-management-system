3312d4d7db26a11c819877502b8a4190cfe052ab
Ahmed Wael <AhmedWael216@protonmail.com>
2022-12-25 15:17:46 +0200

    added stadium manager view

--- routes/stadiumManager.js
+++ routes/stadiumManager.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const adminProcedures = require("../procedures/adminProcedures");
+const stadiumProcedure = require("../procedures/stadiumManagerProcedures");
 const toast = require("../utilities/toast");
 const { authUser, authRole, ROLE } = require("../utilities/auth");
 
 router.get("/", authUser, authRole([ROLE.STADIUM_MANAGER]), function (req, res, next) {  
     res.send("hello World")
+    stadiumProcedure.stadiumManagerViewRelatedInfoOfHisStadium().then((response) => {
+        res.render("stadiumManager/stadiumManagerProfile", {
+            title: "Stadium Manager",
+            username: req.session.username,
+            
+          });
+    });
   });
 
 
