751e9de44cd35719c54c35a41acc230c7c6f2f27
Ahmed Wael <AhmedWael216@protonmail.com>
2022-12-26 14:53:38 +0200

    added upcoming matches to calub representative

--- routes/clubRepresentative.js
+++ routes/clubRepresentative.js
@@ -3,6 +3,30 @@ const router = express.Router();
 const clubRepresentativeProcedures = require("../procedures/clubRepresentativeProcedures");
 const toast = require("../utilities/toast");
 const { authUser, authRole, ROLE } = require("../utilities/auth");
+router.get(
+    "/",
+    authUser,
+    authRole([ROLE.CLUB_REPRESENTATIVE]),
+    async function (req, res, next) {
+        const clubinfo = await clubRepresentativeProcedures.clubRepresentativeViewRelatedInfoOfHisClub(req.session.username)
+        .then((response) => {
+          return response.recordset[0];
+        });
+        const upcomingmatches = await clubRepresentativeProcedures.clubRepresentativeViewUpcomingMatches(req.session.username)
+        .then((response) => {
+          return response.recordset;
+        });
+        console.log("hereeee------------");
+        console.log(upcomingmatches);
+      res.render("clubRepresentative/clubRepresentativeProfile", {
+        title: "club Representative",
+        username: req.session.username,
+        club: clubinfo,
+        matches: upcomingmatches
+      });
+    },
+  );
+
 
 
 module.exports = router;
