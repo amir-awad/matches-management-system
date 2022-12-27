6aadf5d5ae463e374ae342645a2a696b64011056
Ahmed Wael <AhmedWael216@protonmail.com>
2022-12-27 10:10:18 +0200

    changed insertions file

--- database/insertion.sql
+++ database/insertion.sql
@@ -18,22 +18,30 @@ INSERT INTO stadium
 VALUES
     ('Alex Stadium', 'Alex', 10000, 1)
 
-INSERT INTO match
-    (start_time, end_time, host_club_id, guest_club_id, stadium_id)
+Insert INTO users
+    (username,password) 
 VALUES
-    ('2019-01-01 12:00:00', '2019-01-01 13:00:00', 1, 2, 1)
+    ("admin","admin")
+Insert INTO system_admin
+VALUES
+    ("admin","admin")
 
-INSERT INTO ticket
-    (ticket_status, match_id)
-VALUES
-    (1, 1)
-INSERT INTO ticket
-    (ticket_status, match_id)
-VALUES
-    (1, 1)
-INSERT INTO ticket
-    (ticket_status, match_id)
-VALUES
-    (1, 1)
+-- INSERT INTO match
+--     (start_time, end_time, host_club_id, guest_club_id, stadium_id)
+-- VALUES
+--     ('2019-01-01 12:00:00', '2019-01-01 13:00:00', 1, 2, 1)
+
+-- INSERT INTO ticket
+--     (ticket_status, match_id)
+-- VALUES
+--     (1, 1)
+-- INSERT INTO ticket
+--     (ticket_status, match_id)
+-- VALUES
+--     (1, 1)
+-- INSERT INTO ticket
+--     (ticket_status, match_id)
+-- VALUES
+--     (1, 1)
 
 
